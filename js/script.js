57a8adef143a517497dc5f5808003a76cd96f282
Alexey <alexey.derenko@gmail.com>
2019-03-29 09:02:41 +0200

    Диплом 1-ое задание

--- js/script.js
+++ js/script.js
@@ -2,61 +2,69 @@
 
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
-    
-    
-    
-    
-    
+   
+   
+   
+   
+   
     //modal();
-
-
+ 
+ 
     function modal() {
         let modalButton = document.getElementsByTagName('button')[0],
             popupEngineer = document.querySelector('.popup_engineer'),
             popupCloseEngineer = document.querySelector('.popup_engineer .popup_close');
-
+ 
         //модальное окно вызов замерщика
         modalButton.addEventListener('click', function() {
-            popupEngineer.style.display = 'block';
-            document.body.style.overflow = 'hidden';  
-            
-            //console.log(popupClose);
+            popupEngineer.style.display = 'block';           
         });
-
+ 
         //закрытие модальное окно вызов замерщика
         popupCloseEngineer.addEventListener('click', function() {
-            popupEngineer.style.display = 'none'; 
-            document.body.style.overflow = '';  
-            
-            
-            //сделал закрытие на общий клик формы
+            popupEngineer.style.display = 'none';
+            document.body.style.overflow = '';
+          
         });
-
+        
+        //закрытие модального окна по клику на подложку
+        popupEngineer.addEventListener('click', function(event) {
+            if (event.target.classList.contains('popup_engineer')){
+                popupEngineer.style.display = 'none';
+            }
+           
+        });
+ 
+ 
+       
+ 
+ 
+ 
         let phoneLink = document.querySelector('.phone_link'),
             popup = document.querySelector('.popup'),
             popupCloseColl = document.querySelector('.popup .popup_close');
-
+ 
         phoneLink.addEventListener('click', function() {
             popup.style.display = 'block';
-            document.body.style.overflow = 'hidden'; 
+            document.body.style.overflow = 'hidden';
         });
-
+ 
         popupCloseColl.addEventListener('click', function() {
-            popup.style.display = 'none'; 
+            popup.style.display = 'none';
             document.body.style.overflow = '';
         });
-
-
-        $('.overlay,.close').on('click',function(){
-            $('.overlay,.modal').hide();
-        });
-        
-
-        
+ 
+ 
+      
+       
+ 
+       
     }
-
+ 
     modal();
-    
-
-
-});
+   
+ 
+ 
+ });
+ 
+ 
