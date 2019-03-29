e0824e0d4b96bc137c9d6e35639981830fd3643d
Alexey <alexey.derenko@gmail.com>
2019-03-29 09:14:47 +0200

    Добавил модальное окно popup

--- js/script.js
+++ js/script.js
@@ -15,21 +15,21 @@ window.addEventListener('DOMContentLoaded', () => {
             popupEngineer = document.querySelector('.popup_engineer'),
             popupCloseEngineer = document.querySelector('.popup_engineer .popup_close');
  
-        //модальное окно вызов замерщика
+        //модальное окно вызов замерщика 
         modalButton.addEventListener('click', function() {
             popupEngineer.style.display = 'block';           
         });
  
-        //закрытие модальное окно вызов замерщика
+        // закрытие модальное окно на крестик вызов замерщика
         popupCloseEngineer.addEventListener('click', function() {
             popupEngineer.style.display = 'none';
-            document.body.style.overflow = '';
+            //document.body.style.overflow = '';
           
         });
         
         //закрытие модального окна по клику на подложку
         popupEngineer.addEventListener('click', function(event) {
-            if (event.target.classList.contains('popup_engineer')){
+            if (event.target.classList.contains('popup_engineer')) {
                 popupEngineer.style.display = 'none';
             }
            
@@ -43,15 +43,25 @@ window.addEventListener('DOMContentLoaded', () => {
         let phoneLink = document.querySelector('.phone_link'),
             popup = document.querySelector('.popup'),
             popupCloseColl = document.querySelector('.popup .popup_close');
- 
+        
+        //открытие модальное окно - Заказать обратный звонок
         phoneLink.addEventListener('click', function() {
             popup.style.display = 'block';
             document.body.style.overflow = 'hidden';
         });
- 
+        
+        //закрытие модальное окно на крестик - Заказать обратный звонок
         popupCloseColl.addEventListener('click', function() {
             popup.style.display = 'none';
             document.body.style.overflow = '';
+        });
+
+        //закрытие модального окна по клику на подложку-Заказать обратный звонок
+        popup.addEventListener('click', function(event) {
+            if (event.target.classList.contains('popup')) {
+                popup.style.display = 'none';
+            }
+           
         });
  
  
